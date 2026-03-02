4b326f2788776643dcdeaed5ad76d1e66ee55605
Aleksandra <sasahimen11@gmail.com>
2026-03-02 22:46:47 +0700

    2 feature(MainVM): добавлен класс MainVM (6 п)

---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        private Contact _contact = new Contact();
+
+        public Contact Contact
+        {
+            get => _contact;
+            set
+            {
+                _contact = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(PhoneNumber));
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string Name
+        {
+            get => Contact.Name;
+            set
+            {
+                Contact.Name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => Contact.PhoneNumber;
+            set
+            {
+                Contact.PhoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get => Contact.Email;
+            set
+            {
+                Contact.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
