28a0f4956235d8d5c16017ec8de941941ca660f2
Aleksandra <sasahimen11@gmail.com>
2026-03-04 00:14:07 +0700

    3 feature(LoadCommand/SaveComannd): добавлены классы LoadCommand/SaveComannd

--- src/Contacts/View/Model/Services/ContactSerializer.cs
+++ src/Contacts/View/Model/Services/ContactSerializer.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace View.Model.Services
 {
+    /// <summary>
+    /// Сервис для сохранения и загрузки данных контакта.
+    /// </summary>
     public class ContactSerializer
     {
+        /// <summary>
+        /// Путь к файлу сохранения.
+        /// </summary>
+        private static string _path;
+
+        /// <summary>
+        /// Возвращает или задает путь к файлу, в котором хранятся данные.
+        /// </summary>
+        public static string Path
+        {
+            get => _path;
+            set => _path = value;
+        }
+
+        /// <summary>
+        /// Статический конструктор для инициализации пути по умолчанию.
+        /// </summary>
+        static ContactSerializer()
+        {
+            string myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            _path = System.IO.Path.Combine(myDocuments, "Contacts", "contacts.json");
+        }
+
+        /// <summary>
+        /// Сохраняет объект контакта в файл.
+        /// </summary>
+        /// <param name="contact">Экземпляр контакта для сохранения.</param>
+        public static void Save(Contact contact)
+        {
+            string directory = System.IO.Path.GetDirectoryName(_path);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            string json = JsonConvert.SerializeObject(contact);
+            File.WriteAllText(_path, json);
+        }
+
+        /// <summary>
+        /// Загружает объект контакта из файла.
+        /// </summary>
+        /// <returns>Экземпляр контакта. Если файла нет, возвращает новый пустой контакт.</returns>
+        public static Contact Load()
+        {
+            if (!File.Exists(_path))
+            {
+                return new Contact();
+            }
+
+            string json = File.ReadAllText(_path);
+            return JsonConvert.DeserializeObject<Contact>(json);
+        }
     }
 }
